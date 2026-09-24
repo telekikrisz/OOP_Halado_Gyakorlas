b7feee359b3158d311e0bf8b0320c2aee951bed4
kallaikrisz <kallaikrisz@gmail.com>
2026-09-24 10:46:32 +0200

    1.feladat

--- 01_StatikusElemek/Program.cs
+++ 01_StatikusElemek/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _01_StatikusElemek;
+
+Console.WriteLine($"100Eur to HUF: {ValutaValto.EurToHuf(100)}");
+Console.WriteLine($"250USD to HUF: {ValutaValto.UsdToHuf(250)}");
+Console.WriteLine($"Total conversions: {ValutaValto.GetOsszesAtvaltas()}");
