039a064a2589a5e62b81d91f8c352a565849430b
kallaikrisz <kallaikrisz@gmail.com>
2026-09-24 11:36:21 +0200

    3.feladat

---
+++ 03_FeludefinialasEsListak/Program.cs
@@ -0,0 +1,11 @@
+﻿using _03_FeludefinialasEsListak;
+
+Jatekos Bela= new Jatekos("Béla", 5, 100);
+//Console.WriteLine($"Játékos: {Bela.Nev}, Szint: {Bela.Szint}, Pontszám: {Bela.Pontszam}");
+Console.WriteLine(Bela.ToString());
+
+Jatekos Eva = new Jatekos("Éva", 120, 200);
+//Console.WriteLine($"Játékos: {Eva.Nev}, Szint: {Eva.Szint}, Pontszám: {Eva.Pontszam}");
+Console.WriteLine(Eva.ToString());
+
+
